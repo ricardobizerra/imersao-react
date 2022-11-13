6d3e92edc3f4e0a86c85accab862e07307a0a2c3
ricardobizerra <rblf@cin.ufpe.br>
2022-11-13 00:04:10 -0300

    aula 03

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import DarkModeSwitch from "./components/DarkModeSwitch";
 import Search from "./components/Search";
 
 const StyledMenu = styled.header`
@@ -35,6 +36,7 @@ export default function Menu({ valorFiltro, setValorFiltro }) {
         valorFiltro={valorFiltro}
         setValorFiltro={setValorFiltro}
       />
+      <DarkModeSwitch />
     </StyledMenu>
   );
 }
