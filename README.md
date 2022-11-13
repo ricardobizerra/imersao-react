03ba457ccc93677047efa60263cac9ea09c43635
ricardobizerra <rblf@cin.ufpe.br>
2022-11-13 19:32:53 -0300

    aula 04

---
+++ README.md
@@ -0,0 +1,5 @@
+## Como está o nosso projeto?
+
+- `_app.js`: Carrega setup básico do projeto
+    - Aqui vão ficar os Providers de informação do nosso projeto
+    - CSSReset
---
+++ src/components/RegisterVideo/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { StyledRegisterVideo } from "./styles";
+
+function useForm(props) {
+    const [values, setValues] = React.useState(props.initialValues)
+
+    return {
+        values,
+        handleChange: (evento) => {
+            const value = evento.target.value
+            const name = evento.target.name
+            setValues({
+                ...values,
+                [name]: value,
+            })
+        },
+        clearForm() {
+            setValues({});
+        }
+    }
+}
+
+export default function RegisterVideo() {
+    const formCadastro = useForm({
+        initialValues: {
+            titulo: 'aobabiwbvworbo',
+            url: 'ncowibiveonriodotcom'
+        }
+    });
+    const [formVisivel, setFormVisivel] = React.useState(true);
+
+    return (
+        <StyledRegisterVideo>
+            <button className="add-video" onClick={() => setFormVisivel(true)}>
+                +
+            </button>
+
+            {formVisivel
+
+                ? (
+                    <form onSubmit={(evento) => {
+                        evento.preventDefault();
+                        setFormVisivel(false);
+                        formCadastro.clearForm();
+                    }}>
+                        <div>
+                            <button 
+                            type="button"
+                                className="close-modal" 
+                                onClick={() => setFormVisivel(false)}
+                            >
+                                X
+                            </button>
+                            <input 
+                                placeholder="Título do vídeo" 
+                                name="titulo"
+                                value={formCadastro.values.titulo} 
+                                onChange={formCadastro.handleChange}
+                            />
+                            <input 
+                                placeholder="URL" 
+                                name="url"
+                                value={formCadastro.values.url} 
+                                onChange={formCadastro.handleChange}
+                            />
+                            <button type="submit">
+                                Cadastrar
+                            </button>
+                        </div>
+                    </form>
+                )
+
+                : false
+            }
+        </StyledRegisterVideo>
+    )
+}
